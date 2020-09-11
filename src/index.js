58802fc459defa6e01a261183a69c66de7422124
Alexander Markow <alexanderjmarkow@gmail.com>
2020-09-11 09:33:16 -0700

    Update formatting of node and webpack config files.  Add content to HTML and CSS files.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import $ from "jquery";
+// import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import currencyConvert from "./currency.js";
+// import currencyConvert from "./currency.js";
