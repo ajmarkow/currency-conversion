cfa04024b65cb9da334eeef2e79a1875a10f3f85
Alexander Markow <alexanderjmarkow@gmail.com>
2020-09-11 10:59:03 -0700

    Add working api call to busines logic.  Add functo pull data from api response.

--- src/currency.js
+++ src/currency.js
@@ -1,15 +1,23 @@
 export default class currencyConvert {
-  static async conversionRate(startcurrencycode) {
-    try {
-      const response = await fetch(
-        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${startcurrencycode}`
-      );
-      if (!response.ok) {
-        throw Error(response.result);
-      }
-      return response.json;
-    } catch (error) {
-      return error;
+  static conversionRate() {
+    return fetch(
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+    )
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      });
+  }
+  static getCurrencyInfo(response) {
+    if (response) {
+      return response.conversion_rates;
+    } else {
+      return "There was an error fetching the information";
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import currencyConvert from "./currency.js";
 
-let testconversion = currencyConvert.conversionRate("USD");
-console.log(testconversion);
+console.log(currencyConvert.conversionRate());
